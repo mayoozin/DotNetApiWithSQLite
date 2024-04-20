3341165c5b0eb534874467641e4efa5e17cac250
mayoo <mayoozin.developer@gmail.com>
2024-04-21 00:21:40 +0630

    - [ Modified ] Program.cs

--- DotNetApiWithSQLite/Program.cs
+++ DotNetApiWithSQLite/Program.cs
@@ -1,3 +1,5 @@
+using DotNetApiWithSQLite.Controllers;
+using DotNetApiWithSQLite.DbServices;
 using DotNetApiWithSQLite.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IBlogService, BlogService>();
+//builder.Services.AddSingleton<IBlogService, BlogService>();
+builder.Services.AddScoped<BlogService>();
+
+builder.Services.AddScoped(n =>
+{
+    string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sqlite");
+    Directory.CreateDirectory(folderPath);
+    string filePath = Path.Combine(folderPath, builder.Configuration.GetSection("DbFileName").Value!);
+    string connectionString = $"Data Source={filePath};Version=3;";
+    return new SQLiteDbContextService(connectionString);
+});
+
 
 var app = builder.Build();
 
